ce930aa332a7c8eb8835f9382cb64958b3404045
Christopher Zagurski <cjz2501@gmail.com>
2022-02-25 16:51:18 -0600

    Adding bootstrap!

---
+++ scripts/form.js
@@ -0,0 +1,7 @@
+function testResults (form) {
+    var fName = form.fName.value;
+    var lName = form.lName.value;
+    var originalIdea = form.idea.value;
+    var idea = form.videoIdea.value;
+    alert ("First Name: " + fName +"\n Last Name: " + lName + "\n Original Idea: " + originalIdea + "\n Idea: "+ idea);
+}
